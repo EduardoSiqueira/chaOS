040c2479233a94659ac5b4f88f1cc2af2cac81a6
sputnik <sputnik433@gmail.com>
2016-11-07 21:13:16 -0200

    Corrigindo algos

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,17 +1,7 @@
 #ifndef _STDLIB_H
-#define _STDLIB_H 1
- 
-#include <sys/cdefs.h>
- 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#define _STDLIB_H
  
 __attribute__((__noreturn__))
 void abort(void);
- 
-#ifdef __cplusplus
-}
-#endif
  
 #endif
