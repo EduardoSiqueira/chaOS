cdf20aac6fd611aed07972a36328dc20a82e191e
ricardoffv <you@example.com>
2016-11-08 10:37:37 -0200

    teste de limite novo para terminal

--- arch/tty.c
+++ arch/tty.c
@@ -9,7 +9,7 @@
 #include "vga.h"
  
 static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+static const size_t VGA_HEIGHT = 1024;
 static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
  
 static size_t terminal_row;
