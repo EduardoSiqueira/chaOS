e65360d33246ed0043bea76728bced2db84517e9
Ali Husseinat <alihusseinat@gmail.com>
2016-11-16 19:43:39 -0200

    Finaliza a string

---
+++ include/string.h
@@ -0,0 +1,5 @@
+#include <stddef.h>
+int strlen(char * str);
+int strcmp(char * str1, char * str2);
+void memcpy(void * mem1, const void * mem2, size_t n);
+void strcpy(char * str1, const char * str2);
